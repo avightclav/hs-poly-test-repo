1e270e70684512b1057c89f9b237a1e7f8675ffa
avightclav <avightclav@gmail.com>
2022-01-21 02:42:44 +0300

    Part5.Tasks completed

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,29 +4,31 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl func acc [] = acc
+myFoldl func acc (head:tail) = myFoldl func (func acc head) tail
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr func acc [] = acc
+myFoldr func acc (head:tail) = func head (myFoldr func acc tail)
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap func = myFoldr (\elem acc -> func elem:acc) []
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap func = myFoldr (\elem acc -> func elem ++ acc) []
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myFoldr (++) []
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = foldl (flip (:)) []
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldr (\elem acc -> if p elem then elem : acc else acc) []
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
+myPartition p = myFoldr (\elem (accTrue, accFalse) -> if p elem then (elem : accTrue, accFalse) else (accTrue, elem : accFalse)) ([], [])
 
